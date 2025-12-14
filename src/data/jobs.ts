886620873435eca9f3f3aea94fff70459e37889b
Ken <kzou55@gmail.com>
2025-12-14 11:11:47 -0500

    Initialized the inital job data constants

--- src/data/jobs.ts
+++ src/data/jobs.ts
@@ -1,12 +1,26 @@
-export const projects: Project[] = [
+import type { Job } from "./types";
+
+export const jobs: Job[] = [
     {
-        name:,
-        logo:,
-        description:,
-        startDate:,
-        endDate:,
-        skills:,
-        links:,
-        repoLinks: 
+        company: "Generate Product Development",
+        logo: "",
+        dateStart: "September 2022",
+        dateEnd: "December 2022",
+        description: " ",
+        techStack: [],
+        companyLink: "https://generatenu.com/",
+        jobTitle: "Build Studio Software Engineer",
+    },
+    {
+        company: "Healthedge(Formerly Wellframe)",
+        logo: "",
+        dateStart: "January 2022",
+        dateEnd: "August 2022",
+        description: "A digital health platform and mobile app that connects health plan members with their care teams",
+        techStack: [
+
+        ],
+        companyLink: "https://www.wellframe.com/",
+        jobTitle: "Software QA Automation Engineer Co-op",
     }
 ]
